e8e37bae6f274e398193df684a1e1d9445313c33
bakedrice14 <ahmadfauzirizky003@gmail.com>
2022-11-29 12:32:43 +0700

    finish uts

---
+++ script.js
@@ -0,0 +1,12 @@
+//navbar
+const nav = document.querySelector('.navbar');
+window.onscroll = function () { 
+    var top = window.scrollY;
+    console.log(top);
+    if (top >= 680) {
+        nav.classList.add('activated')
+    }
+    else { 
+        nav.classList.remove('activated');
+    }
+}
